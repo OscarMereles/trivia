3d972d5940fff5dedcb8c7a1866fe69ddea4cfc0
Oscar Mereles <oscar.mereles97@gmail.com>
2026-03-11 17:18:16 -0300

    se hizo hasta una pregunta

---
+++ trivia.py
@@ -0,0 +1,6 @@
+puntos=0
+nombre=input("ingrese nombre ")
+print(f" HOLA ,{nombre} vamos a jugar una trivia")
+primera=input("Cual es el valor mas proximo de pi? A: 3     B: 3,14     C: 3,141592      D: 2,7")
+if (primera=="C" or primera=="c"):
+    puntos+=1
